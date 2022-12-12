915f7046381a57e4c78c67742631072b0dcd2f8e
YeonsuBaek <soothbugger@gmail.com>
2022-12-12 23:36:17 +0900

    refactor(baseball2): 고정된 값을 상수로 모듈화

--- src/retry/model/RandomNumberMaker.js
+++ src/retry/model/RandomNumberMaker.js
@@ -1,10 +1,11 @@
 const { Random } = require('@woowacourse/mission-utils');
+const VALUE = require('../constant/value');
 
 const RandomNumberGenerator = {
   generate() {
     const numbers = [];
-    while (numbers.length < 3) {
-      const number = Random.pickNumberInRange(1, 9);
+    while (numbers.length < VALUE.LENGTH) {
+      const number = Random.pickNumberInRange(VALUE.RANGE_MINIMUM, VALUE.RANGE_MAXIMUM);
       if (!numbers.includes(number)) {
         numbers.push(number);
       }
