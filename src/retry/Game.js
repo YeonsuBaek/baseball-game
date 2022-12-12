1a674209b85f53436bf61a91fbea05fda1091fac
YeonsuBaek <soothbugger@gmail.com>
2022-12-12 15:13:11 +0900

    feat(baseball2): 컴퓨터 숫자 랜덤 고르기 기능 구현

--- src/retry/Game.js
+++ src/retry/Game.js
@@ -1,8 +1,19 @@
 const OutputView = require('./OutputView');
+const Computer = require('./Computer');
 
 class Game {
+  #computer;
+
+  constructor() {
+    this.#computer;
+  }
   start() {
     OutputView.printStart();
+    this.getNumbers();
+  }
+
+  getNumbers() {
+    this.#computer = new Computer().getNumbers();
   }
 }
 
