2fa5b73435e3943b30f2346c89f6e9196922c761
YeonsuBaek <soothbugger@gmail.com>
2022-12-12 15:27:42 +0900

    feat(baseball2): 사용자가 숫자를 입력하는 기능 구현

--- src/retry/Game.js
+++ src/retry/Game.js
@@ -1,11 +1,14 @@
 const OutputView = require('./OutputView');
+const InputView = require('./InputView');
 const Computer = require('./Computer');
 
 class Game {
   #computer;
+  #user;
 
   constructor() {
     this.#computer;
+    this.#user;
   }
   start() {
     OutputView.printStart();
@@ -14,7 +17,14 @@ class Game {
 
   getNumbers() {
     this.#computer = new Computer().getNumbers();
+    InputView.getNumbers(this.handleUserNumbers);
   }
+
+  handleUserNumbers = (numbers) => {
+    this.#user = numbers.split('').map((number) => {
+      return parseInt(number, 10);
+    });
+  };
 }
 
 module.exports = Game;
